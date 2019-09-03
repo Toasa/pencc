fb4876a19e5a86e59a4decfb378a7ca06c0d56c3
Toasa <atsushi.tohyama.160.333@gmail.com>
2019-09-03 08:53:43 +0000

    Add tokenize()

--- main.c
+++ main.c
@@ -1,29 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum enm {
+    TK_INT,
+    TK_ADD,
+    TK_SUB,
+    TK_EOF,
+} TokenKind;
+
+typedef struct Token {
+    TokenKind kind;
+    int val;
+    struct Token *next;
+} Token;
+
 char *input;
-
-void add();
-
-void gen_assembly() {
-    int n = num();
-    printf("        mov rax, %d\n", n);
-    add();
-}
-
-// 100+99
-void add() {
-    while (*input == '+' || *input == '-') {
-        int m;
-        if (*input++ == '+') {
-            m = num();
-            printf("        add rax, %d\n", m);
-        } else {
-            m = num();
-            printf("        sub rax, %d\n", m);
-        }
-    }
-}
 
 int num() {
     int n = *input++ - '0';
@@ -39,6 +30,65 @@ int isDigit() {
         return 0;
 }
 
+void skip() {
+    while (*input == ' ') {
+        input++;
+    }
+}
+
+Token *newToken(TokenKind kind, int val) {
+    Token *t = malloc(sizeof(Token));
+    t->kind = kind;
+    t->val = val;
+    return t;
+}
+
+Token *tokenize() {
+    skip();
+
+    Token *head_token = malloc(sizeof(Token));
+    Token *cur_token = malloc(sizeof(Token));
+    Token *new_token = malloc(sizeof(Token));
+    Token *tail_token = malloc(sizeof(Token));
+
+    head_token = newToken(TK_INT, num());
+    cur_token = head_token;
+
+    while (*input) {
+        skip();
+        if (isDigit()) {
+            new_token = newToken(TK_INT, num());
+        } else {
+            if (*input++ == '+') {
+                new_token = (Token *)newToken(TK_ADD, 0);
+            } else {
+                new_token = newToken(TK_SUB, 0);
+            }
+        }
+        cur_token->next = new_token;
+        cur_token = new_token;
+    }
+
+    tail_token = newToken(TK_EOF, 0);
+    cur_token->next = tail_token;
+
+    return head_token;
+}
+
+void gen_assembly(Token *t) {
+    printf("        mov rax, %d\n", t->val);
+
+    for (t = t->next; t->kind != TK_EOF; t = t->next) {
+        if (t->kind == TK_ADD) {
+            t = t->next;
+            printf("        add rax, %d\n", t->val);
+        } else {
+            t = t->next;
+            printf("        sub rax, %d\n", t->val);
+        }        
+    }
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "Need to 2 arguments\n");
@@ -50,7 +100,9 @@ int main(int argc, char **argv) {
     printf("main:\n");
 
     input = argv[1];
-    gen_assembly();
+    Token *token_head = tokenize();
+
+    gen_assembly(token_head);
 
     printf("        ret\n");
     return 0;
