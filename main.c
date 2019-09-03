aab42fb6bb170c67661b7913657b56d0bd7c8f81
Toasa <atsushi.tohyama.160.333@gmail.com>
2019-09-03 01:01:40 +0000

    2桁以上の整数の足し算と引き算を実装

--- main.c
+++ main.c
@@ -1,11 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *input;
+
+void add();
+
+void gen_assembly() {
+  int n = num();
+  printf("        mov rax, %d\n", n);
+  add();
+}
+
+// 100+99
+void add() {
+  while (*input == '+' || *input == '-') {
+    int m;
+    if (*input++ == '+') {
+      m = num();
+      printf("        add rax, %d\n", m);
+    } else {
+      m = num();
+      printf("        sub rax, %d\n", m);
+    }
+  }
+}
+
+int num() {
+  int n = *input++ - '0';
+  while (isDigit())
+    n = 10 * n + (*input++ - '0');
+  return n;
+}
+
+int isDigit() {
+  if ('0' <= *input && *input <= '9')
+    return 1;
+  else 
+    return 0;
+}
+
 int main(int argc, char **argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Need to 2 arguments\n");
+    return 1;
+  }
+
   printf(".intel_syntax noprefix\n");
   printf(".global main\n\n");
   printf("main:\n");
-  printf("    mov rax, %d\n", atoi(argv[1]));
-  printf("    ret\n");
+
+  // printf("    mov rax, %d\n", atoi(argv[1]));
+  input = argv[1];
+  gen_assembly();
+
+  printf("        ret\n");
   return 0;
 }
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 try() {
-    expected="$1"
-    input="$2"
+    input="$1"
+    expected="$2"
 
     ./main "$input" > tmp.s
     gcc -o tmp tmp.s
@@ -12,7 +12,7 @@ try() {
     if [ $actual = $expected ]; then
         echo "${input} => ${actual}"
     else
-        echo "{$expected} expected, but got {$actual}"
+        echo "${expected} expected, but got ${actual}"
         exit 1
     fi
 }
@@ -20,3 +20,9 @@ try() {
 try 1 1
 try 100 100
 try 255 255
+
+try "3+4" 7
+try "100-10" 90
+try "1+20+3-4+30-1" 49
+
+echo OK
